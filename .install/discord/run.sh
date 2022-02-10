5ff3310be4793543756b4fe263d6d3a5bd3969e4
BoLLYde Maxime <ozeliurs@gmail.com>
2022-02-10 16:40:09 +0100

    Update run.sh

--- .install/discord/run.sh
+++ .install/discord/run.sh
@@ -1,1 +1,9 @@
+DIR=`dirname "$0"`
 
+if [[ -f "$DIR/Discord.deb" ]]
+then
+    echo "Downloading Discord ..."
+    wget "https://discord.com/api/download?platform=linux&format=deb" -O Discord.deb
+fi
+
+dpkg -i --ignore-depends=libgconf-2-4,libappindicator1,libc++1 $DIR/discord.deb
