9a598cd831b167d4d97d3c41d525b06c98cbe56a
BoLLYde Maxime <ozeliurs@gmail.com>
2022-02-24 15:33:15 +0100

    Update run.sh

--- .install/minecraft/run.sh
+++ .install/minecraft/run.sh
@@ -2,8 +2,8 @@ DIR=`dirname "$0"`
 
 if [ ! -f "$DIR/Minecraft.deb" ]
 then
-    echo "Downloading Minecraft ..."
-    wget "https://launcher.mojang.com/download/Minecraft.deb" -O Minecraft.deb
+echo "Downloading Minecraft ..."
+wget "https://launcher.mojang.com/download/Minecraft.deb" -O Minecraft.deb
 fi
 
 dpkg -i $DIR/Minecraft.deb
