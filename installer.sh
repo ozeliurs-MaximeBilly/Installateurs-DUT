e5c434db19ac65e61431daa070d08ba2f810d812
BoLLYde Maxime <ozeliurs@gmail.com>
2022-02-25 12:35:41 +0100

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,3 +1,12 @@
 DIR=`dirname "$0"`
 mv $DIR/.install/ $HOME/
 chmod 777 -R $HOME/.install
+
+while true; do
+    read -p "Do you wish to install this program?" yn
+    case $yn in
+        [Yy]* ) make install; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
